99ef6f935c684192aa38b99b02beabb488c1947f
Mudassir Shabbir <mudassir@agoric.com>
2024-09-28 15:47:36 +0500

    adding fundme app

---
+++ test/Fundme.js
@@ -0,0 +1,97 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Fundme", function () {
+    let Fundme, fundme, owner, addr1, addr2, addrs;
+
+    beforeEach(async function () {
+        Fundme = await ethers.getContractFactory("Fundme");
+        [owner, addr1, addr2, addr3, ...addrs] = await ethers.getSigners();
+        fundme = await Fundme.deploy([owner.address, addr1.address, addr2.address, addr3.address], 2);
+        await fundme.waitForDeployment();
+    });
+
+describe("Deployment", function () {
+        it("Should set the right owners", async function () {
+            expect(await fundme.getOwners()).to.deep.equal([owner.address, addr1.address, addr2.address, addr3.address]);
+        });
+
+        it("Should set the right number of confirmations required", async function () {
+            expect(await fundme.numConfirmationsRequired()).to.equal(2);
+        });
+    });
+
+describe("Owners", function () {
+        it("Should print all owners", async function () {
+            const owners = await fundme.getOwners();
+            console.log("Owners:", owners);
+            expect(owners).to.deep.equal([owner.address, addr1.address, addr2.address, addr3.address]);
+        });
+    });
+
+describe("Transactions", function () {
+        it("Should submit a transaction", async function () {
+            await fundme.submitTransaction(addr1.address, 100, "0x");
+            const tx = await fundme.getTransaction(0);
+            expect(tx.to).to.equal(addr1.address);
+            expect(tx.value).to.equal(100);
+            expect(tx.executed).to.be.false;
+            expect(tx.numConfirmations).to.equal(0);
+        });
+
+        it("Should confirm a transaction", async function () {
+            await fundme.submitTransaction(addr1.address, 100, "0x");
+            await fundme.confirmTransaction(0);
+            const tx = await fundme.getTransaction(0);
+            expect(tx.numConfirmations).to.equal(1);
+        });
+
+        it("Should execute a transaction", async function () {
+            await fundme.submitTransaction(addr1.address, 100, "0x");
+            await fundme.confirmTransaction(0);
+            await fundme.connect(addr3).confirmTransaction(0);
+            await fundme.connect(addr1).confirmTransaction(0);
+            await fundme.connect(addr1).executeTransaction(0);
+            const tx = await fundme.getTransaction(0);
+            expect(tx.executed).to.be.true;
+        });
+
+        it("Should revoke a confirmation", async function () {
+            await fundme.submitTransaction(addr1.address, 100, "0x");
+            await fundme.confirmTransaction(0);
+            await fundme.revokeConfirmation(0);
+            const tx = await fundme.getTransaction(0);
+            expect(tx.numConfirmations).to.equal(0);
+        });
+
+        it("Should not execute a transaction without enough confirmations", async function () {
+            await fundme.submitTransaction(addr1.address, 100, "0x");
+            await fundme.confirmTransaction(0);
+            await expect(fundme.executeTransaction(0)).to.be.revertedWith("cannot execute tx");
+        });
+    });
+
+    describe("Modifiers", function () {
+        it("Should revert if not owner tries to submit transaction", async function () {
+            await expect(fundme.connect(addrs[0]).submitTransaction(addr1.address, 100, "0x")).to.be.revertedWith("not owner");
+        });
+
+        it("Should revert if transaction does not exist", async function () {
+            await expect(fundme.confirmTransaction(0)).to.be.revertedWith("tx does not exist");
+        });
+
+        it("Should revert if transaction already executed", async function () {
+            await fundme.submitTransaction(addr1.address, 100, "0x");
+            await fundme.confirmTransaction(0);
+            await fundme.connect(addr1).confirmTransaction(0);
+            await fundme.executeTransaction(0);
+            await expect(fundme.executeTransaction(0)).to.be.revertedWith("tx already executed");
+        });
+
+        it("Should revert if transaction already confirmed", async function () {
+            await fundme.submitTransaction(addr1.address, 100, "0x");
+            await fundme.confirmTransaction(0);
+            await expect(fundme.confirmTransaction(0)).to.be.revertedWith("tx already confirmed");
+        });
+    });
+});
